f7dbd3f782ef5f75c0eebffcc6c8b37b6f730aa5
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-03-28 00:23:17 -0400

    Finished replacing phenny name with bitbot

--- modules/arb_control.py
+++ modules/arb_control.py
@@ -4,26 +4,26 @@ btc-arbitrage module
 Kyle Fitzsimmons 2013, http://kylefitz.com/
 """
 import os
-from BitcoinArbitrage import control
+from BitcoinArbitrage import irc_control
 
-def arb_balance(phenny, input):
+def arb_balance(bitbot, input):
     if input == ".balance":
-        phenny.say("Getting balances from all exchanges")
+        bitbot.say("Getting balances from all exchanges")
         markets = ['mtgx','bflr','bstp','bctl']
     else:
         markets = [ input.split(" ", 1)[1] ] # remove ".balance" command from string
 
     for market in markets:
 	    try:
-		    balance_object = control.get_balance(market)
+		    balance_object = irc_control.get_balance(market)
 		    usd_float = round(balance_object.usd_balance, 4)
 		    btc_float = round(balance_object.btc_balance, 4)
 		    usd = " " * (8-len(str(usd_float))) + str(usd_float)
 		    btc = " " * (7-len(str(btc_float))) + str(btc_float)
 		    balance_output = market + " > USD: " + usd + " | BTC: " + btc
-		    phenny.say(balance_output)
+		    bitbot.say(balance_output)
 	    except:
-		    phenny.say("Please check exchange initialism.")
+		    bitbot.say("Please check exchange initialism.")
 
 arb_balance.commands = ['balance', 'bal']
 arb_balance.name = 'arb_balance'
--- modules/arb_txs.py
+++ modules/arb_txs.py
@@ -4,26 +4,26 @@ btc-arbitrage module
 Kyle Fitzsimmons 2013, http://kylefitz.com/
 """
 import os
-from BitcoinArbitrage.irc_scripts import txs
+from BitcoinArbitrage import irc_control
 
-def arb_balance(phenny, input):
+def arb_balance(bitbot, input):
     if input == ".txs":
-        phenny.say("Getting transactions from all exchanges")
+        bitbot.say("Getting transactions from all exchanges")
         markets = ['mtgx','bflr','bstp','bctl']
     else:
         markets = [ input.split(" ", 1)[1] ] # remove ".txs" command from string
 
     for market in markets:
 	    try:
-		    balance_object = balance.get_balance(market)
+		    balance_object = irc_control.txs(market)
 		    usd_float = round(balance_object.usd_balance,4)
 		    btc_float = round(balance_object.btc_balance,4)
 		    usd = " " * (8-len(str(usd_float))) + str(usd_float)
 		    btc = " " * (7-len(str(btc_float))) + str(btc_float)
 		    balance_output = market + " > USD: " + usd + " | BTC: " + btc
-		    phenny.say(balance_output)
+		    bitbot.say(balance_output)
 	    except:
-		    phenny.say("Please check exchange initialism.")
+		    bitbot.say("Please check exchange initialism.")
 
 arb_txs.commands = ['txs']
 arb_txs.name = 'arb_txs'
