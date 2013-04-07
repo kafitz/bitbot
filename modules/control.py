55db0f2d2c16a7b8781b93d1b796ce51cbb78074
bas <ted@mint>
2013-04-07 13:27:17 +0200

    timedelta added

--- modules/control.py
+++ modules/control.py
@@ -346,6 +346,7 @@ deal.name = 'deal'
 
 def stats(bitbot, input):
     import sqlite3
+    import datetime
     db = 'opportunties_database.db'
     conn = sqlite3.connect(db) 
     cursor = conn.cursor()  
@@ -354,17 +355,20 @@ def stats(bitbot, input):
     for bm in markets:
         for sm in markets:
             d = {}
-            d['buy market'] = bm
-            d['sell market'] = sm
-            cursor.execute("SELECT COUNT(*),AVG(profit), AVG(ratio), MAX(profit), MAX(ratio) FROM deals WHERE lower(sell_market)=? AND lower(buy_market)=?",[sm,bm])
-            d['deals'], d['avg profit'], d['avg ratio'], d['max profit'], d['max ratio']  = cursor.fetchone()
+            d['buy market'], d['sell market'] = bm, sm
+            cursor.execute("SELECT COUNT(*),AVG(profit), AVG(ratio), MAX(profit), MAX(ratio), MAX(time) FROM deals WHERE lower(sell_market)=? AND lower(buy_market)=?",[sm,bm])
+            d['deals'], d['avg profit'], d['avg ratio'], d['max profit'], d['max ratio'], d['timestring']  = cursor.fetchone()
+            if d['timestring'] != None:
+                d['time'] = datetime.datetime.strptime(d['timestring'], "%Y-%m-%d %H:%M:%S")
+                d['timedelta'] =datetime.datetime.utcnow() - d['time']
+                d['timedeltastring'] = str(d['timedelta'].days) + ' days, ' + str(d['timedelta'].seconds/3600) + ' hours'
             l.append(d)
-    bitbot.say('{0:11} => {1:11} | {2:4} | {3:5} | {4:5} | {5:5} | {6:5}'\
-           .format('buy market','sell market','#','avg $', 'max $', 'avg %', 'max %'))
+    bitbot.say('{0:11} => {1:11} | {2:4} | {3:5} | {4:5} | {5:5} | {6:5} | {7}'\
+           .format('buy market','sell market','#','avg $', 'max $', 'avg %', 'max %', 'happened for the last time'))
     for d in l:
         if d['deals'] != 0:
-            bitbot.say('{0:11} => {1:11} | {2:4} | {3:.3f} | {4:.3f} | {5:.3f} | {6:.3f}'\
-                   .format(d['buy market'],d['sell market'],d['deals'],d['avg profit'],d['max profit'],d['avg ratio'],d['max ratio']))
+            bitbot.say('{0:11} => {1:11} | {2:4} | {3:.3f} | {4:.3f} | {5:.3f} | {6:.3f} | {7} ago'\
+                   .format(d['buy market'],d['sell market'],d['deals'],d['avg profit'],d['max profit'],d['avg ratio'],d['max ratio'],d['timedeltastring']))
     return
     
 stats.commands = ['stats']
