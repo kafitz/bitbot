24add9212d1be7ac5fd527cd4d17501130c612de
bas <ted@mint>
2013-04-08 23:43:52 +0200

    started cpbx api, done: .open, .bal; the rest is hard to test without funds

--- config.py
+++ config.py
@@ -1,41 +1,41 @@
 ##### Arbitrage output
-arbitrage_output = "#botbottest"
-deal_output = "#botbottest"
+arbitrage_output = '#merlin-spam'
+deal_output = '#botbottest'
 
 # watch the following markets
-# markets = ["bitfloorUSD", "MtGoxUSD", "BtceUSD", "BitstampUSD", "CampBXUSD", "Bitcoin24USD", "VircurexUSD"]
-markets = ["BitfloorUSD", "MtGoxUSD", "BitstampUSD", "CampBXUSD"]
-# private_markets = {"bflr": "Bitfloor", "mtgx": "MtGox", "bstp": "Bitstamp", "bctl": "BitcoinCentral"}
-private_markets = {"bstp": "Bitstamp", "mtgx": "MtGox", "bflr": "Bitfloor"}
+# markets = ['bitfloorUSD', 'MtGoxUSD', 'BtceUSD', 'BitstampUSD', 'CampBXUSD', 'Bitcoin24USD', 'VircurexUSD']
+markets = ['BitfloorUSD', 'MtGoxUSD', 'BitstampUSD', 'CampBXUSD']
+# private_markets = {'bflr': 'Bitfloor', 'mtgx': 'MtGox', 'bstp': 'Bitstamp', 'bctl': 'BitcoinCentral'}
+private_markets = {'bstp': 'Bitstamp', 'mtgx': 'MtGox', 'bflr': 'Bitfloor', 'cpbx': 'CampBX'}
 
-# buy_markets = {"bflr": "Bitfloor", "btce": "BTCe", "bc24": "Bitcoin24"}
-# sell_markets = {"bstp": "Bitstamp", "mtgx": "MtGox"}
+# buy_markets = {'bflr': 'Bitfloor', 'btce': 'BTCe', 'bc24': 'Bitcoin24'}
+# sell_markets = {'bstp': 'Bitstamp', 'mtgx': 'MtGox'}
 
 # observers if any
-# ["Logger", "TraderBot", "TraderBotSim", "HistoryDumper", "Emailer", "Database"]
-observers = ["Logger", "Database"]
+# ['Logger', 'TraderBot', 'TraderBotSim', 'HistoryDumper', 'Emailer', 'Database']
+observers = ['Logger', 'Database']
 
 
 #### Trader Bot Config
 # Access to Private APIs
-mtgox_key = "a224882a-5884-402a-af08-dd58bc59f1a0"
-mtgox_secret = "JO4j70yJhttMjypG8A/TeqkdtMV/aC+37c+t5mvjMy13wLU4ymFG0F52hECjEpKREAoQl4qkrdbgBGxMFIdSUg=="
+mtgox_key = 'a224882a-5884-402a-af08-dd58bc59f1a0'
+mtgox_secret = 'JO4j70yJhttMjypG8A/TeqkdtMV/aC+37c+t5mvjMy13wLU4ymFG0F52hECjEpKREAoQl4qkrdbgBGxMFIdSUg=='
 
-bitcoincentral_username = "fitzatschool@gmail.com"
-bitcoincentral_password = "wmitAY5h*3%NHvX0"
+bitcoincentral_username = 'fitzatschool@gmail.com'
+bitcoincentral_password = 'wmitAY5h*3%NHvX0'
 
-bitstamp_user = "27368"
-bitstamp_password = "1TOsEZms3yaRxNoH"
+bitstamp_user = '27368'
+bitstamp_password = '1TOsEZms3yaRxNoH'
 
-bitfloor_key = "e27dcdb8-8388-4227-bfc0-ca641c3bc797"
-bitfloor_secret = "puEtkb4qab/68Tx6o1wnsKuypQClg9P4a1OrjSlbjpzmn+l9/HslL2lzY1Z4SU3CMbRyuBlaDRaLeAl5b/HAug=="
+bitfloor_key = 'e27dcdb8-8388-4227-bfc0-ca641c3bc797'
+bitfloor_secret = 'puEtkb4qab/68Tx6o1wnsKuypQClg9P4a1OrjSlbjpzmn+l9/HslL2lzY1Z4SU3CMbRyuBlaDRaLeAl5b/HAug=='
 bitfloor_passphrase = 'dP7Wmoa*G8WAIU2S'
 
-btce_key = "G9N9IB1J-RKQA0K45-B1CLR6IT-4A1QQYLX-WJG9OXD5"
-btce_secret = "9ae4b00b25b9d7e285dcfac11caf6924da029e858bad4b5b06c210405a3dd587"
+btce_key = 'G9N9IB1J-RKQA0K45-B1CLR6IT-4A1QQYLX-WJG9OXD5'
+btce_secret = '9ae4b00b25b9d7e285dcfac11caf6924da029e858bad4b5b06c210405a3dd587'
 
-bitcoin24_user = "crooton"
-bitcoin24_key = "CXftzt6uzwYXuh5eZbRrKCJwRqGxUR37"
+bitcoin24_user = 'crooton'
+bitcoin24_key = 'CXftzt6uzwYXuh5eZbRrKCJwRqGxUR37'
 
 # SafeGuards
 max_amount = 0.43  # in BTC
---
+++ private_markets/campbx.py
@@ -0,0 +1,178 @@
+from market import Market
+import datetime
+import urllib
+import urllib2
+import sys
+import json
+from decimal import Decimal
+
+class PrivateCampBX(Market):
+    name = 'CampBX'
+    trade_url = {'method': 'POST', 'url': 'https://campbx.com/api/tradeenter.php'}
+    open_orders_url = {'method': 'POST', 'url': 'https://campbx.com/api/myorders.php'}
+    tx_url = {'method': 'POST', 'url': 'https://campbx.com/api/user_transactions/'}
+    info_url = {'method': 'POST', 'url': 'https://campbx.com/api/myfunds.php'}
+    deposit_url = {'method': 'POST', 'url': 'https://campbx.com/api/getbtcaddr.php'}  
+    withdraw_url = {'method': 'POST', 'url': 'https://campbx.com/api/sendbtc.php'}
+    cancel_url = {'method': 'POST', 'url': 'https://campbx.com/api/tradecancel.php'}  
+
+    def __init__(self):
+        super(Market, self).__init__()
+        self.user = self.config.campbx_user
+        self.password = self.config.campbx_password
+        self.currency = 'USD'
+        self.error = ''
+        self.last_opportunity = None
+        
+    def _format_time(self,timestamp):
+        return datetime.datetime.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+
+    def _send_request(self, url, params, extra_headers=None):
+        if extra_headers is not None:
+            for k, v in extra_headers.iteritems():
+                headers[k] = v
+        req = urllib2.Request(url['url'], urllib.urlencode(params))
+        print params
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.HTTPError, e:
+            return json.loads(e.read())
+        else:
+            jsonstr = json.loads(response.read())
+            return jsonstr
+        return 0
+
+    def trade(self, amount, trademode, price):
+        params = {'user': self.user,
+                  'pass': self.password,
+                  'Quantity': amount,
+                  'TradeMode' : str(trademode),
+                  'Price': price}
+        response = self._send_request(self.trade_url, params)
+        print response
+        if response and 'Error' not in response:
+            self.price = str(response['price'])
+            self.id = str(response['id'])
+            self.timestamp = str(response['datetime'])
+            self.amount = str(response['amount'])
+            return 1
+        elif response and 'Error' in response:
+            self.error = str(response['Error'])
+            return 1
+        return 0
+
+    def buy(self, amount, price):
+        return self.trade(amount, 'QuickBuy', price)
+
+    def sell(self, amount, price):
+        return self.trade(amount, 'QuickSell', price)
+
+    def get_info(self):
+        params = {'user': self.user, 'pass': self.password}
+        response = self._send_request(self.info_url, params)
+        print response
+        if response and 'Error' not in response:
+            self.usd_balance = float(response['Total USD'])
+            self.btc_balance = float(response['Total BTC'])
+            self.fee = '0.55'
+            return 1
+        elif response and 'Error' in response:
+            self.error = str(response['Error'])
+            return 1
+        return 0
+
+    def get_txs(self):
+        self.error = 'this API can\'t list transactions'
+        return 1
+
+    def get_orders(self):
+        params = {'user': self.user, 'pass': self.password}
+        response = self._send_request(self.open_orders_url, params)
+        self.orders_list = []
+        print response
+        if response and response['Sell'][0]['Info'] == 'No open Sell Orders' and response['Buy'][0]['Info'] == 'No open Buy Orders':
+            self.error = 'no open orders listed'
+            return 1
+        if response and response['Sell'][0]['Info'] != 'No open Sell Orders':
+            for order in response['Sell']:
+                o = {}
+                o['type'] = 'sell'
+                o['timestamp'] = str(order['datetime'])
+                o['price'] = '$' + str(order['price']) + ' USD/BTC'
+                o['amount'] = str(round(float(order['amount']),1)) + ' BTC'
+                o['id'] = str(order['id'])
+                self.orders_list.append(o)
+        if response and response['Buy'][0]['Info'] != 'No open Buy Orders':   
+                o = {}
+                o['type'] = 'buy'
+                o['timestamp'] = str(order['datetime'])
+                o['price'] = '$' + str(order['price']) + ' USD/BTC'
+                o['amount'] = str(round(float(order['amount']),1)) + ' BTC'
+                o['id'] = str(order['id'])
+                self.orders_list.append(o)             
+        if response and 'Error' in response:
+            self.error = str(response['Error'])
+            return 1
+        return 0
+
+    def cancel(self, order_id):
+        self.get_orders()
+        if len(self.orders_list) == 0:
+            self.error = 'no open orders listed'
+            return 1
+        for order in self.orders_list:
+            if order_id == order['id']:
+                order_type = order['type']
+                self.cancelled_amount = order['amount']
+        try:
+            order_type = order_type
+        except:
+            self.error = 'order does not exist'
+            return 1
+
+        params = [('user', self.user),
+                  ('pass', self.password),
+                  ('OrderID', order_id),
+                  ('Type', order_type)]
+        response = self._send_request(self.cancel_url, params)
+        
+        if response:
+            self.cancelled_id = order_id
+            self.cancelled_time = str(datetime.datetime.now()).split('.')[0]
+            return 1
+        elif response:
+            self.error = str(response['error'])
+            return 1
+        return 0
+
+    def withdraw(self, amount, address):
+        params = {'user': self.user, 
+                  'pass': self.password, 
+                  'BTCAmt': str(amount), 
+                  'BTCTo': str(address)}
+        response = self._send_request(self.withdraw_url, params)
+        if response and 'Error' not in response:
+            self.timestamp = str(datetime.datetime.now())
+            return 1
+        elif response and 'Error' in response:
+            self.error = str(response['Error'])
+            return 1
+        return 0
+
+    def deposit(self):
+        params = {'user': self.user, 'pass': self.password}
+        response = self._send_request(self.deposit_url, params)
+        if response and 'Success' in response:
+            self.address = response['Success']
+            return 1
+        elif response and 'Error' in response:
+            self.error = str(response['Error'])
+            return 1
+        return 0
+
+    def get_lag(self):
+        self.error = 'not available from this API'
+        return 1
+
+if __name__ == '__main__':
+    CampBX = PrivateCampBX()
