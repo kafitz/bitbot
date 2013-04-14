59673a3b5248ace73ef2e5441d3e73ecf93a6498
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-04-14 01:01:04 -0400

    Deal output message cleanup

--- observers/traderbot.py
+++ observers/traderbot.py
@@ -52,6 +52,7 @@ class TraderBot(Observer):
         if gox_lag > 60:
             bitbot.msg(config.deal_output, "MtGox lag of {}, too risky to trade")
         else:
+            bitbot.msg(config.deal_output, "MtGox lag (seconds): " + str(gox_lag))
             print "MtGox lag (seconds): " + str(gox_lag)
             self.execute_trade(bitbot, deals)
 
@@ -99,7 +100,7 @@ class TraderBot(Observer):
                 ignored_exchange = buy_mkt
             if sell_mkt in self.ignore_exchange:
                 ignored_exchange = sell_mkt
-            output = "#{0} deal skipped because {1} exchange ignored".format(trade_attempt, ignored_exchange)
+            output = "#{0} {1} ignored".format(trade_attempt, ignored_exchange)
             self.failed_outputs.append(output)
             best_deal_index += 1
             self.execute_trade(bitbot, deals, best_deal_index)
@@ -125,7 +126,7 @@ class TraderBot(Observer):
             return
         # test 3
         if percent_profit < config.profit_thresh:
-            output = "#{0} minimum percent profit ({4:.2f}%) between {1} and {2} not reached: {3:.2f}%".format(trade_attempt, buy_mkt, sell_mkt, percent_profit, config.profit_thresh)
+            output = "#{0} min. % {1} -> {2}: {3:.2f}/{4:.2f}%".format(trade_attempt, buy_mkt, sell_mkt, percent_profit, config.profit_thresh)
             logging.warn(output)
             self.failed_outputs.append(output)
             best_deal_index += 1
@@ -145,7 +146,7 @@ class TraderBot(Observer):
             self.execute_trade(bitbot, deals, best_deal_index)
             return
         if buy_tradeable_amt < config.trade_amount:
-            output = "#{0} insufficient balance (${1} USD) to execute trade at {2}".format(trade_attempt, self.clients[buy_mkt].usd_balance, buy_mkt)
+            output = "#{0} {2} balance: ${1} USD".format(trade_attempt, self.clients[buy_mkt].usd_balance, buy_mkt)
             self.ignore_exchange.append(buy_mkt)
             self.failed_outputs.append(output)
             logging.warn(output)
@@ -153,7 +154,7 @@ class TraderBot(Observer):
             self.execute_trade(bitbot, deals, best_deal_index)
             return
         if sell_tradeable_amt < config.trade_amount:
-            output = "#{0} insufficient balance ({1} BTC) to execute trade at {2}".format(trade_attempt, self.clients[sell_mkt].btc_balance, sell_mkt)
+            output = "#{0} {2} balance: {1} BTC".format(trade_attempt, self.clients[sell_mkt].btc_balance, sell_mkt)
             self.ignore_exchange.append(sell_mkt)
             self.failed_outputs.append(output)
             logging.warn(output)
